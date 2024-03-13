5bb42f5b108e2db98de46ddff89ad32e2f230a63
SimonLee <lixiaomin_235@163.com>
2024-03-13 22:43:55 +0800

    update docker push scripts

--- push_ci.sh
+++ push_ci.sh
@@ -3,4 +3,8 @@
 WORK_DIR=$(dirname "$(readlink -f "$0")")
 source $WORK_DIR/conf/setup_env.sh
 
+# Note:
+#   Need to docker login first with your dockerhub username & password
+
+# Push to docker hub
 docker push $CI_TAG
