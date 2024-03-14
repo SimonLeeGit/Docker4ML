be65d8c8b164218548b23f2ccd5aa944e1efcd30
SimonLee <lixiaomin_235@163.com>
2024-03-14 20:50:00 +0800

    update conf

--- conf/setup_env.sh
+++ conf/setup_env.sh
@@ -3,7 +3,7 @@ TAG=ml:dev
 CI_TAG=ml:ci
 
 # Base docker image tag used by docker build
-BASE_IMG=nvcr.io/nvidia/pytorch:24.01-py3
+BASE_IMG=nvcr.io/nvidia/pytorch:24.02-py3
 
 # User name used in docker container
 USER_NAME=developer
